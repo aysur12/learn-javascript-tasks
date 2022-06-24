8acc91e8648917a0df0e602e8f844e95b1e9aafc
Aysur <aysursabin@gmail.com>
2022-06-24 15:58:06 +0700

    solved tasks of chapter 8.2

---
+++ 8200-f-prototype/8200-f-prototype.js
@@ -0,0 +1,28 @@
+/* Создайте новый объект с помощью уже существующего
+https://learn.javascript.ru/task/new-object-same-constructor
+*/
+
+function Animal(name) {
+  this.name = name;
+}
+const rabbit = new Animal('Bunny');
+const monkey = new rabbit.constructor('Chucky');
+
+console.log(rabbit.name);
+console.log(monkey.name);
+
+// Пример функции-конструктора, с которой такой код поведёт себя неправильно
+
+function Animal(name) {
+  this.name = name;
+}
+
+Animal.prototype = {};
+
+const rabbit = new Animal('Bunny');
+const monkey = new rabbit.constructor('Chucky');
+
+console.log(rabbit.name);
+console.log(monkey.name);
+
+
