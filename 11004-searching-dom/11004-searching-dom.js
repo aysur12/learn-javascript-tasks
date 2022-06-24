d9c96e0e6ff9a01a82e6c4cba483bdf27d109d23
Aysur <aysursabin@gmail.com>
2022-06-24 22:28:38 +0700

    solved tasks of chapter 1.4 of part 2

---
+++ 11004-searching-dom/11004-searching-dom.js
@@ -0,0 +1,17 @@
+// Поиск элементов https://learn.javascript.ru/task/find-elements
+
+const tableAge = document.getElementById('age-table');
+
+const tableLabels = document.querySelectorAll('#age-table label');
+
+const firstTd = tableAge.querySelector('td');
+
+const searchForm = document.querySelector('form[name="search"]');
+
+const firstInput = searchForm.querySelector('input');
+
+const  formInputs = form.querySelectorAll('input');
+
+const lastInput = formInputs[inputs.length-1];
+
+
