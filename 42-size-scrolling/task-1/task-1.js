4d7d6b7f86a424d73e5fea45c2d288aea5024022
Aysur <aysursabin@gmail.com>
2022-06-25 22:27:27 +0700

    solved tasks of chapter 1.9 of part 2

---
+++ 42-size-scrolling/task-1/task-1.js
@@ -0,0 +1,3 @@
+// Найти размер прокрутки снизу https://learn.javascript.ru/task/get-scroll-height-bottom
+
+const scrollBottom = elem.scrollHeight - elem.scrollTop - elem.clientHeight;
---
+++ 42-size-scrolling/task-2/task-2.js
@@ -0,0 +1,3 @@
+// Узнать ширину полосы прокрутки https://learn.javascript.ru/task/scrollbar-width
+
+const scrollWidth = elem.offsetWidth - elem.clientWidth;
